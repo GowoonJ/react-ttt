8d2b19907149d5df005ad1f27f7c68fab822def5
gowoonJ <jgw972229@gmail.com>
2023-05-31 19:16:36 +0900

    feat(tailwind): set tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Game from './App.jsx'
+import Header from './header'
 import './index.scss'
 
 const root = document.getElementById('root')
 createRoot(root).render(
   <StrictMode>
+    <Header />
     <Game clss="tictactoe game" />
   </StrictMode>
 )
